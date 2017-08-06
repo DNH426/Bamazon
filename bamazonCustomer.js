ed84aa7ba37e0be3f56b0c531586d3b730dc798f
unknown <hagydaniel@gmail.com>
2017-08-06 17:10:06 -0500

    I just realized my pushes were not working through VSC

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,70 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+// Installed a table module where products will be displayed
+var Table = require("cli-table");
 
+// Connect to MySQL 
 var connection = mysql = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Chicago820!",
     database: "bamazon"
-})
+});
+
+// Connect to MySQL database 
+connection.connect(function (err) {
+    if (err) throw err;
+    // Create a callback function 
+    displayData(function () {
+        start();
+    });
+});
+
+// Display table with inventory
+function displayData(data) {
+    var table = new Table({
+        head: ['ID Number', 'Product', 'Department', 'Price', 'Quantity Available']
+    });
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+        // Loop through inventory and display each item within the table
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price.toFixed(2), res[i].stock_quantity])
+        }
+        console.log(table.toString());
+        data();
+    });
+}
+
+// Use the callback function
+function start() {
+    // Make room between table and the welcome message
+    console.log('\n');
+    inquirer.prompt([{
+        name: 'userChoice',
+        message: 'Welcome to Bamazon! Would you like to shop or exit the store?',
+        type: 'rawlist',
+        choices: ['Shop', 'Exit']
+    }]).then(function (answers) {
+        if (answers.userChoice === 'Shop') {
+            viewStore();
+        } else {
+            console.log('\n');
+            console.log('Come back soon!');
+        }
+    })
+}
+
+// Create a function that allows the user to purchase items
+function viewStore() {
+    inquirer.prompt([{
+        name: 'ID',
+        type: 'input',
+        message: 'Please enter the ID of the item you would like to purchase',
+    }, {
+        name: 'quantity',
+        type: 'input',
+        message: 'Please input the quantity of the items you want to purchase'
+    }])
+};
--- bamazonProducts.sql
+++ bamazonProducts.sql
@@ -12,41 +12,41 @@ CREATE TABLE products(
 item_id INT AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
-price DECIMAL NOT NULL,
+price DECIMAL(10,2) NOT NULL,
 stock_quantity INT NOT NULL,
 PRIMARY KEY (item_id)
 );
 
 -- Insert 10 items into table
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('Shaun of the Dead', 'DVD', '12', '75');
+VALUES('Shaun of the Dead', 'DVD', '12.99', '75');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('The Blues Brothers', 'DVD', '10', '50');
+VALUES('The Blues Brothers', 'DVD', '10.15', '50');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('Fight Club', 'DVD', '15', '42');
+VALUES('Fight Club', 'DVD', '15.99', '42');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('The Goonies', 'DVD', '18', '36');
+VALUES('The Goonies', 'DVD', '18.98', '36');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('Pulp Fiction', 'DVD', '10', '3');
+VALUES('Pulp Fiction', 'DVD', '10.99', '3');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('Me, Myself & Ireen', 'DVD', '11', '29');
+VALUES('Me, Myself & Ireen', 'DVD', '11.55', '29');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('The Dark Knight', 'DVD', '8', '85');
+VALUES('The Dark Knight', 'DVD', '8.01', '85');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('Se7en', 'DVD', '3', '42');
+VALUES('Se7en', 'DVD', '3.02', '42');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('The Usual Suspects', 'DVD', '1', '10');
+VALUES('The Usual Suspects', 'DVD', '1.03', '10');
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES('Saving Private Ryan', 'DVD', '20', '2');
+VALUES('Saving Private Ryan', 'DVD', '20.10', '2');
 
 -- Show me the table in MySQL Workbench
 SELECT * FROM products;
